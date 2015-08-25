3407a7f642b3e9e59e7d3ed438bd44344fe6b724
dmnlk <seikima2demon@gmail.com>
2015-08-26 01:46:15 +0900

    Add Layer

---
+++ Classes/TopPageScene.cpp
@@ -0,0 +1,9 @@
+//
+//  TopPageScene.cpp
+//  SampleCocos
+//
+//  Created by dmnlk on 2015/08/26.
+//
+//
+
+#include "TopPageScene.h"
---
+++ Classes/TopPageScene.h
@@ -0,0 +1,20 @@
+//
+//  TopPageScene.h
+//  SampleCocos
+//
+//  Created by dmnlk on 2015/08/26.
+//
+//
+
+#ifndef __SampleCocos__TopPageScene__
+#define __SampleCocos__TopPageScene__
+
+#include <cocos2d.h>
+
+class TopPageLayer : public cocos2d::Layer
+{
+    
+};
+
+
+#endif /* defined(__SampleCocos__TopPageScene__) */
