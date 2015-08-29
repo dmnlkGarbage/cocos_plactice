035d5ec3d0e3cdce8ee9ac09525e5e3c5154ef34
dmnlk <seikima2demon@gmail.com>
2015-08-30 01:29:06 +0900

    add label

--- Classes/TopPageScene.cpp
+++ Classes/TopPageScene.cpp
@@ -18,6 +18,8 @@ bool TopPageLayer::init() {
     
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
-
+    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
     return true;
 }
