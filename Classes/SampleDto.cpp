492921e4988d853bc67a8ee95e227fa122328f0c
dmnlk <seikima2demon@gmail.com>
2015-09-22 01:08:36 +0900

    add sample dto

---
+++ Classes/SampleDto.cpp
@@ -0,0 +1,9 @@
+//
+//  SampleDto.cpp
+//  SampleCocos
+//
+//  Created by 川口将貴 on 2015/09/22.
+//
+//
+
+#include "SampleDto.h"
---
+++ Classes/SampleDto.h
@@ -0,0 +1,14 @@
+//
+//  SampleDto.h
+//  SampleCocos
+//
+//  Created by 川口将貴 on 2015/09/22.
+//
+//
+
+#ifndef __SampleCocos__SampleDto__
+#define __SampleCocos__SampleDto__
+
+#include <stdio.h>
+
+#endif /* defined(__SampleCocos__SampleDto__) */
