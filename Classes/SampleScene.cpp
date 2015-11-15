5738ee182633b41249735831f24d42520c9ef4b0
dmnlk <seikima2demon@gmail.com>
2015-11-16 00:05:20 +0900

    new class

---
+++ Classes/SampleScene.cpp
@@ -0,0 +1,9 @@
+//
+//  SampleScene.cpp
+//  SampleCocos
+//
+//  Created by 川口将貴 on 2015/11/16.
+//
+//
+
+#include "SampleScene.hpp"
---
+++ Classes/SampleScene.hpp
@@ -0,0 +1,14 @@
+//
+//  SampleScene.hpp
+//  SampleCocos
+//
+//  Created by 川口将貴 on 2015/11/16.
+//
+//
+
+#ifndef SampleScene_hpp
+#define SampleScene_hpp
+
+#include <stdio.h>
+
+#endif /* SampleScene_hpp */
