a3107d67fa1429be6d26c13f029a8ac97aa40faf
dmnlk <seikima2demon@gmail.com>
2016-01-06 00:37:33 +0900

    edit boxsample

--- Classes/SampleScene.cpp
+++ Classes/SampleScene.cpp
@@ -8,6 +8,7 @@
 
 #include "SampleScene.hpp"
 
+
 cocos2d::Scene* SampleScene::createScene()
 {
     // 'scene' is an autorelease object
@@ -21,4 +22,88 @@ cocos2d::Scene* SampleScene::createScene()
     
     // return the scene
     return scene;
+}
+
+bool SampleScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+    
+    // add a "close" icon to exit the progress. it's an autorelease object
+//    auto closeItem = MenuItemImage::create(
+//                                           "CloseNormal.png",
+//                                           "CloseSelected.png",
+//                                           CC_CALLBACK_1(HelloWorld::menuCloseCallback, this));
+//    
+//    closeItem->setPosition(Vec2(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+//                                origin.y + closeItem->getContentSize().height/2));
+//    
+//    // create menu, it's an autorelease object
+//    auto menu = Menu::create(closeItem, NULL);
+//    menu->setPosition(Vec2::ZERO);
+//    this->addChild(menu, 1);
+    
+    /////////////////////////////
+    // 3. add your codes below...
+    
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+    
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+    
+    // add "HelloWorld" splash screen"
+//    auto sprite = Sprite::create("HelloWorld.png");
+//    
+//    // position the sprite on the center of the screen
+//    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+//    
+//    // add the sprite as a child to this layer
+//    this->addChild(sprite, 0);
+    
+    
+///    cocos2d::extension::EditBox* editbox  = EditBox::cre
+    
+    auto editBox = EditBox::create(Size(100, 50), Scale9Sprite::create("bg.png"));
+    editBox->setFont("fonts/Marker Felt.ttf", 12.0f);
+    editBox->setPlaceHolder("ここに入力してください");
+    editBox->setPlaceholderFontColor(Color3B::BLACK);
+    editBox->setFontColor(Color3B::BLACK);
+    editBox->setMaxLength(100);
+    editBox->setText("初期設定文字\ntest");
+    editBox->setReturnType(EditBox::KeyboardReturnType::DONE);
+    editBox->setInputMode(EditBox::InputMode::ANY);
+    editBox->cocos2d::Node::setPosition(100, 200);
+    editBox->setDelegate(this);
+    this->addChild(editBox);
+    return true;
+}
+
+void SampleScene::editBoxEditingDidBegin(EditBox *editBox) {
+}
+
+void SampleScene::editBoxEditingDidEnd(EditBox *editBox) {
+}
+
+void SampleScene::editBoxTextChanged(EditBox *editBox, const std::string& text) {
+}
+
+void SampleScene::editBoxReturn(EditBox *editBox) {
 }
--- Classes/SampleScene.hpp
+++ Classes/SampleScene.hpp
@@ -11,12 +11,22 @@
 #define SampleScene_hpp
 
 #include "cocos2d.h"
+#include "extensions/cocos-ext.h"
+USING_NS_CC;
+using namespace ui;
+//USING_NS_CC_EXT;
 
-class SampleScene :public cocos2d::Layer {
+class SampleScene :public cocos2d::Layer, public ui::EditBoxDelegate {
 public:
     static cocos2d::Scene* createScene();
+    virtual bool init();
 
     CREATE_FUNC(SampleScene);
+private:
+    virtual void editBoxEditingDidBegin(EditBox *editBox);
+    virtual void editBoxEditingDidEnd(EditBox *editBox);
+    virtual void editBoxTextChanged(EditBox *editBox, const std::string& text);
+    virtual void editBoxReturn(EditBox *editBox);
 };
 
 #endif /* SampleScene_hpp */
